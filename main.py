14906d9dca938f8a725a3edbd1301406e62c5ceb
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-12 00:51:44 +0000

    added dataset and layers

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-import pandas as pandas
-import tensorflow as tf 
+import pandas 
+import tensorflow 
+
+dataset = pandas.read_csv('/Users/oliverjohnson/loan-eligibility-predictor/loan-train.csv')
+x = dataset.drop(columns=['Loan_Status'])
+y = dataset['Loan_Status']
+
+model = tensorflow.keras.models.Sequential()
+
+model.add(tensorflow.keras.Input(shape=(x.shape)))
+
+#input layers
+model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid')) 
+#hidden layers
+model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid')) 
+#output layer
+model.add(tensorflow.keras.layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
+
