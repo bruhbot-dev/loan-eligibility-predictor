8748a58c4e190bfc798e74ba1cb7b7313a7ab443
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-12 12:38:05 +0000

    tried using the output of train_test_split to solve error, no luck

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pandas 
 import tensorflow 
+from sklearn.model_selection import train_test_split
+
 
 dataset = pandas.read_csv('/Users/oliverjohnson/loan-eligibility-predictor/loan-train.csv')
 x = dataset.drop(columns=['Loan_Status'])
 y = dataset['Loan_Status']
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01)
+
 model = tensorflow.keras.models.Sequential()
 
-model.add(tensorflow.keras.Input(shape=(x.shape)))
+model.add(tensorflow.keras.Input(shape=(x_train.shape)))
 
 #input layers
 model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid')) 
@@ -17,7 +21,7 @@ model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid'))
 model.add(tensorflow.keras.layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
 
-model.fit(x,y, epochs=1000)
+model.fit(x_train,y_train, epochs=1000)
 
 
 
