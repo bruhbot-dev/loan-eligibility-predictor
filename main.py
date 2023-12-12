d4b55479d2bf5e4279bd34df90c6aebc3329e0be
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-12 21:55:29 +0000

    .

--- main.py
+++ main.py
@@ -26,18 +26,18 @@ model = tensorflow.keras.models.Sequential()
 model.add(tensorflow.keras.Input(shape=(x_train_scaled.shape[1])))
 
 #input layers
-model.add(tensorflow.keras.layers.Dense(256, activation='relu')) 
+model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid')) 
 #hidden layers
-model.add(tensorflow.keras.layers.Dense(128, activation='relu')) 
-model.add(tensorflow.keras.layers.Dense(64, activation='relu')) 
-model.add(tensorflow.keras.layers.Dense(32, activation='relu')) 
-model.add(tensorflow.keras.layers.Dense(16, activation='relu')) 
-model.add(tensorflow.keras.layers.Dense(8, activation='relu')) 
+model.add(tensorflow.keras.layers.Dense(128, activation='sigmoid')) 
+model.add(tensorflow.keras.layers.Dense(64, activation='sigmoid')) 
+model.add(tensorflow.keras.layers.Dense(32, activation='sigmoid')) 
+model.add(tensorflow.keras.layers.Dense(16, activation='sigmoid')) 
+model.add(tensorflow.keras.layers.Dense(8, activation='sigmoid')) 
 
 
 #output layer
-model.add(tensorflow.keras.layers.Dense(1, activation='sigmoid'))
-model.compile(loss = "categorical_crossentropy", optimizer = opt,metrics=['accuracy'])
+model.add(tensorflow.keras.layers.Dense(1, activation='relu'))
+model.compile(loss = "binary_crossentropy", optimizer = "adam",metrics=['accuracy'])
 model.fit(x_train_scaled,y_train, epochs=300, batch_size=32,validation_split=0.2)
 
 model.evaluate(x_test_scaled,y_test)
