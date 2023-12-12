052f1abb4f30868289cd1efbdeee27aa8658c3a4
bruhbot-dev <olivergeorgejames@gmail.com>
2023-12-12 13:10:46 +0000

    got rid of test train since it was not the source of the issue

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import pandas 
 import tensorflow 
-from sklearn.model_selection import train_test_split
 
 
 dataset = pandas.read_csv('/Users/oliverjohnson/loan-eligibility-predictor/loan-train.csv')
 x = dataset.drop(columns=['Loan_Status'])
 y = dataset['Loan_Status']
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.01)
 
 model = tensorflow.keras.models.Sequential()
 
-model.add(tensorflow.keras.Input(shape=(x_train.shape)))
+model.add(tensorflow.keras.Input(shape=(x.shape)))
 
 #input layers
 model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid')) 
@@ -20,8 +18,7 @@ model.add(tensorflow.keras.layers.Dense(256, activation='sigmoid'))
 #output layer
 model.add(tensorflow.keras.layers.Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])
-
-model.fit(x_train,y_train, epochs=1000)
+model.fit(x,y, epochs=1000)
 
 
 
